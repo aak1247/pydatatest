f6e1ff79a56aadacc4b95f4031d75c9067cf26bf
aak1247 <aak1247@126.com>
2022-08-21 17:38:07 +0800

    readme

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name= 'pydatatest',
@@ -12,10 +15,10 @@ setup(
     author= 'aak1247',
     author_email='aak1247@126.com',
     url='https://github.com/aak1247/PyDataTest',
-
     description= 'Data driven test framework',
-
+    long_description=read('README'),
     install_requires=[
-        "pyyaml"
+        "pyyaml",
+        #"requests", # optional, only if you want to use session, it is needed
     ],
 )
